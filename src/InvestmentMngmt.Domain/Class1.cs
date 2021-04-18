f6a8ae96adfaf5b1be688dffc109aa86bc95d521
Zaquiel Grings <zaquiel@gmail.com>
2021-04-18 18:14:26 -0300

    Creation of the project structure

---
+++ src/InvestmentMngmt.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace InvestmentMngmt.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/InvestmentMngmt.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace InvestmentMngmt.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
